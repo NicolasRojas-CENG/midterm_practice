35d6bbd8866ab030e416b59e5246229de2acbf1f
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2019-10-29 15:49:29 -0400

    ...

--- app/src/main/java/com/test/MainActivity.java
+++ app/src/main/java/com/test/MainActivity.java
@@ -3,26 +3,12 @@ package com.test;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Toolbar;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity{
 
-    private static final int REQUEST_CODE = 2000;
-    private Menu menu;
-    private boolean mclicked = false;
-    Toolbar mToolbar;
-    FloatingActionButton mFab;
-    FloatingActionButton mFab1;
-    EditText mediText2;
-    TextView mtextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +26,7 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
-    mFab.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
 
-        }
-    });
 
     }
 }
