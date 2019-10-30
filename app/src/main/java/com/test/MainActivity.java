209cb68f7d42e44c05a2e8592ba666fcf754c648
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2019-10-29 20:14:24 -0400

    Added a menu bar.

--- app/src/main/java/com/test/MainActivity.java
+++ app/src/main/java/com/test/MainActivity.java
@@ -3,6 +3,9 @@ package com.test;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -25,9 +28,44 @@ public class MainActivity extends AppCompatActivity{
                 startActivity(mainScreenIntent);
             }
         });
-
-
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.item1:
+                finishAndRemoveTask();
+                return true;
+
+            case R.id.item2:
+                Toast.makeText(getApplicationContext(),"Selected item2.", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.item3:
+                Toast.makeText(getApplicationContext(),"Selected item3.", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.submenuitem1:
+                Toast.makeText(getApplicationContext(), "Going to main screen.", Toast.LENGTH_LONG).show();
+                //Intent mainScreenIntent = new Intent(MainActivity.this, main_screen.class);
+                Intent mainScreenIntent = main_screen.makeIntent(MainActivity.this);
+                startActivity(mainScreenIntent);
+                return true;
+
+            case R.id.submenuitem2:
+                Toast.makeText(getApplicationContext(), "Exiting app.", Toast.LENGTH_SHORT).show();
+                finishAndRemoveTask();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
 
