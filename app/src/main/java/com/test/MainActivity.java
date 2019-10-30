08d0f1e59d00651cf17a61541a09440967d965a0
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2019-10-29 23:02:34 -0400

    Added two floating buttons to the start screen (test activity).

--- app/src/main/java/com/test/MainActivity.java
+++ app/src/main/java/com/test/MainActivity.java
@@ -1,8 +1,12 @@
 package com.test;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -10,14 +14,50 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity{
 
-
+FloatingActionButton actionButtonMail;
+FloatingActionButton actionButtonAlert;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        actionButtonMail = findViewById(R.id.fab);
+        actionButtonMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Do you want to send message?", Snackbar.LENGTH_LONG)
+                        .setAction("TEXT", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                email();
+                            }
+                        }).show();
+            }
+        });
+
+        actionButtonAlert = findViewById(R.id.fab1);
+        actionButtonAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Alert Dialog.").setMessage("This is an alert message.")
+                        .setIcon(R.drawable.ic_warning)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .create();
+                dialog.show();
+            }
+        });
+
         Button test = findViewById(R.id.button1);
         test.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,5 +108,20 @@ public class MainActivity extends AppCompatActivity{
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    private void email(){
+
+        try{
+            String message = "hello";
+            Intent sendIntent = new Intent(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, message);
+            sendIntent.setType("text/plain");
+            startActivity(sendIntent);
+        }catch (SecurityException error)
+        {
+            Log.e("MapleLeaf", "Call Failed");
+        }
+    }
+
 }
 
